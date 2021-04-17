7e538dec8c2c7e86aa4ecfe91b3e296e02f03e5b
Benjamín de la Fuente Ranea <yosoymin@gmail.com>
2021-04-17 21:27:10 +0200

    WIP: creating textures in a shared context

--- source/main.cpp
+++ source/main.cpp
@@ -2,65 +2,93 @@
 #include <GLFW/glfw3.h>
 
 #include <stdio.h>
+#include <array>
 
 namespace {
-   void errorCallback(int error, const char* description) {
-      fprintf(stderr, "GLFW error %d: %s\n", error, description);
-   }
+    void errorCallback(int error, const char* description) {
+        fprintf(stderr, "GLFW error %d: %s\n", error, description);
+    }
 
-   GLFWwindow* initialize() {
-      int glfwInitRes = glfwInit();
-      if (!glfwInitRes) {
-         fprintf(stderr, "Unable to initialize GLFW\n");
-         return nullptr;
-      }
+    GLFWwindow* initialize() {
+        int glfwInitRes = glfwInit();
+        if (!glfwInitRes) {
+            fprintf(stderr, "Unable to initialize GLFW\n");
+            return nullptr;
+        }
 
-      glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-      glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-      glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
-      glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-      GLFWwindow* window = glfwCreateWindow(1280, 720, "InitGL", nullptr, nullptr);
-      if (!window) {
-         fprintf(stderr, "Unable to create GLFW window\n");
-         glfwTerminate();
-         return nullptr;
-      }
+        GLFWwindow* window = glfwCreateWindow(1280, 720, "InitGL", nullptr, nullptr);
+        if (!window) {
+            fprintf(stderr, "Unable to create GLFW window\n");
+            glfwTerminate();
+            return nullptr;
+        }
 
-      glfwMakeContextCurrent(window);
+        glfwMakeContextCurrent(window);
 
-      int gladInitRes = gladLoadGL();
-      if (!gladInitRes) {
-         fprintf(stderr, "Unable to initialize glad\n");
-         glfwDestroyWindow(window);
-         glfwTerminate();
-         return nullptr;
-      }
+        int gladInitRes = gladLoadGL();
+        if (!gladInitRes) {
+            fprintf(stderr, "Unable to initialize glad\n");
+            glfwDestroyWindow(window);
+            glfwTerminate();
+            return nullptr;
+        }
 
-      return window;
-   }
+        return window;
+    }
+}
+
+// This function creates a texture and return his GL id
+GLuint createTexture(unsigned width, unsigned height) {
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+    return texture;
+}
+
+// This function creates some textures in a shared context and deletes them in the main context
+// This is generating some memory leaks in GPU memory in Intel drivers since version TODO: set version found that reproduces this bug
+void testSharedContextTextures(GLFWwindow* mainWindow, GLFWwindow* sharedContextWindow) {
+    glfwMakeContextCurrent(sharedContextWindow);
+
+    std::array<GLuint, 2> textures;
+    for (auto& texture : textures)
+        texture = createTexture(1024, 1024);
+
+    glfwMakeContextCurrent(mainWindow);
+
+    glDeleteTextures(textures.size(), textures.data());
 }
 
 int main(int argc, char* argv[]) {
-   glfwSetErrorCallback(errorCallback);
+    glfwSetErrorCallback(errorCallback);
 
-   GLFWwindow* window = initialize();
-   if (!window) {
-      return 0;
-   }
+    GLFWwindow* window = initialize();
+    if (!window) {
+        return 0;
+    }
 
-   // Set the clear color to a nice green
-   glClearColor(0.15f, 0.6f, 0.4f, 1.0f);
+    // Set the clear color to a nice green
+    glClearColor(0.15f, 0.6f, 0.4f, 1.0f);
 
-   while (!glfwWindowShouldClose(window)) {
-      glClear(GL_COLOR_BUFFER_BIT);
+    GLFWwindow* sharedContextWindow = glfwCreateWindow(1, 1, "", nullptr, window);
 
-      glfwSwapBuffers(window);
-      glfwPollEvents();
-   }
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
 
-   glfwDestroyWindow(window);
-   glfwTerminate();
+        testSharedContextTextures(window, sharedContextWindow);
 
-   return 0;
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
