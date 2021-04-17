4f5cb3669dcd7e4640ed4c25a0a3637f07c28f29
Benjamín de la Fuente Ranea <yosoymin@gmail.com>
2021-04-17 21:02:22 +0200

    update platform

--- source/main.cpp
+++ source/main.cpp
@@ -16,8 +16,8 @@ namespace {
       }
 
       glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-      glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-      glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+      glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
       glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
       GLFWwindow* window = glfwCreateWindow(1280, 720, "InitGL", nullptr, nullptr);
